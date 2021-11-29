9991a35c2e984dc3e6eeba80a5e397b3c11d87eb
Mandicifu <al292486@uji.es>
2021-11-29 23:22:48 +0000

    Impl task2

---
+++ js/task2.js
@@ -0,0 +1,6 @@
+// Input two number that are multiplied together and presented
+let number1 = prompt("Give me a number:");
+let number2 = prompt("Give me another number:");
+
+let multiplied = number1 * number2
+alert(multiplied);
