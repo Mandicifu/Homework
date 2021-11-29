b122e4c209f0e1ea72978046391018f8d9449e82
Mandicifu <al292486@uji.es>
2021-11-29 23:26:21 +0000

    cosmetic change to task2

--- js/task2.js
+++ js/task2.js
@@ -1,6 +1,9 @@
-// Input two number that are multiplied together and presented
+// Two prompts with inputs
 let number1 = prompt("Give me a number:");
 let number2 = prompt("Give me another number:");
 
+// Multiply the inputs together
 let multiplied = number1 * number2
+
+// Alert the result
 alert(multiplied);
